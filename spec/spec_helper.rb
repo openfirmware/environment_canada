b5485ff367d0ba2154fae8cf48404000085e5f61
James Badger <jamesbadger@gmail.com>
2013-10-07 20:19:08 -0600

    Add VCR and WebMock gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require 'environment_canada'
+
 require 'pry'
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
