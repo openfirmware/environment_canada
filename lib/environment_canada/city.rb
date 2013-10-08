acb603420222fcdbbb017a41d586f4a3d37770ae
James Badger <jamesbadger@gmail.com>
2013-10-07 20:03:36 -0600

    Add city find using stored list

--- lib/environment_canada/city.rb
+++ lib/environment_canada/city.rb
@@ -2,8 +2,16 @@ module EnvironmentCanada
   class City
     # Class Methods
     class << self
+      # Return hash of cities with city codes as keys and city names as values.
+      def cities
+        @cities ||= YAML.load_file(File.expand_path('../../city_list_en.yml', __FILE__))['cities']
+      end
+
       # Return list of cities that match a keyword.
       def find(keyword)
+        cities.find_all do |city_code, city_name|
+          city_name == keyword
+        end
       end
     end
 
--- spec/lib/city_spec.rb
+++ spec/lib/city_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe EnvironmentCanada::City do
   it "finds a set of cities that match a term" do
-    pending
     result = EnvironmentCanada::City.find("Calgary")
     result.should respond_to(:each)
   end
