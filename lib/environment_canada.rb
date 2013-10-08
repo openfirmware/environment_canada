fe868f3efa07c3ca737ee9f40bad2b3f0da4b265
James Badger <jamesbadger@gmail.com>
2013-10-07 20:03:16 -0600

    Enable YAML safe mode loading

--- lib/environment_canada.rb
+++ lib/environment_canada.rb
@@ -3,6 +3,8 @@ require "safe_yaml"
 require "environment_canada/city"
 require "environment_canada/version"
 
+SafeYAML::OPTIONS[:default_mode] = :safe
+
 module EnvironmentCanada
   # Your code goes here...
 end
